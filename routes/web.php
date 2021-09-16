6d6ca278869fb27fbf8b8bacf6b0215baf7c4068
Calex <carlosalexandergomezmolina@gmail.com>
2021-09-16 17:40:42 -0500

    route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 Route::post('/pros_login', [App\Http\Controllers\LoginController::class, 'login'])->name('pros_login');
 Route::get('/cerrar_sesion', [App\Http\Controllers\LoginController::class, 'logout'])->name('cerrar_sesion');
 
+//ROUTES
 Route::group(['middleware' => 'session'], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::post('/data', [App\Http\Controllers\HomeController::class, 'data'])->name('data');
